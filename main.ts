e518d45fc09e41937d15440f663b799f8d1edf3b
Luke Barnard <luke@lukebarnard.co.uk>
2024-09-11 14:28:37 +0200

    Implement a setting that hides the task ID completely or behind an emoji

--- main.ts
+++ main.ts
@@ -1,87 +1,34 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Plugin, PluginSettingTab, Setting } from 'obsidian';
+
+import { hideIDsExtension } from 'extension';
 
 // Remember to rename these classes and interfaces!
 
 interface MorgenPluginSettings {
-//	mySetting: string;
+	decorateIDs: 'show' | 'hide' | 'replace_with_emoji'
 }
 
 const DEFAULT_SETTINGS: MorgenPluginSettings = {
-//	mySetting: 'default'
+	decorateIDs: 'show'
 }
 
 export default class MorgenPlugin extends Plugin {
 	settings: MorgenPluginSettings;
 
 	async onload() {
-		/*
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+		this.registerEditorExtension(hideIDsExtension(this.settings));
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-		*/
+		this.addSettingTab(new MorgenSettingTab(this.app, this));
 	}
 
-	onunload() {
+	onunload() { }
 
+	async updateSettings(update: Partial<MorgenPluginSettings>) {
+		Object.assign(this.settings, update);
+		return this.saveSettings()
 	}
 
 	async loadSettings() {
@@ -93,24 +40,7 @@ export default class MorgenPlugin extends Plugin {
 	}
 }
 
-/*
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
+class MorgenSettingTab extends PluginSettingTab {
 	plugin: MorgenPlugin;
 
 	constructor(app: App, plugin: MorgenPlugin) {
@@ -124,15 +54,17 @@ class SampleSettingTab extends PluginSettingTab {
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+			.setName('Enabled task ID hiding')
+			.setDesc('Hide all IDs in your note to reduce noise')
+		.addDropdown((component) => {
+			component
+			.addOptions({
+				replace_with_emoji: 'Replace with emoji',
+				hide: 'Hide completely',
+				show: 'Show IDs',
+			})
+			.setValue(this.plugin.settings.decorateIDs)
+			.onChange((value: typeof this.plugin.settings.decorateIDs) => this.plugin.updateSettings({decorateIDs : value}))
+		})
 	}
 }
-*/
